e9902a4cf674aae2268db044be856ab5433dbf00
Indigo253931 <indigo253931@gmail.com>
2016-08-13 19:01:43 -0600

    challenge 2 setup and exercise 1 complete

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,21 @@
 console.log('challenge #2 -- app.js loaded!');
 
  var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+ app.controller("WelcomeController", WelcomeController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+ function WelcomeController(){
+   this.full_name="Monica";
+   this.age="19";
+   this.city="Temecula"
+   this.state="California";
+   this.name_count="6";
+   this.shout="Monica!";
+ }
+
+ function WDIController(){
+   this.class_name = "White Walkers";
+   this.enrolled_students="15";
+   this.start_date="June 13th";
+   this.end_date="September 3rd";
+   this.daysRemaining="21";
+ }
