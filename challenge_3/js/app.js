8438c9c75301d2a4417b8f4c8d76f800aa094e68
Indigo253931 <indigo253931@gmail.com>
2016-08-14 21:32:12 -0600

    added loan controller to js file

--- challenge_3/js/app.js
+++ challenge_3/js/app.js
@@ -2,12 +2,13 @@ console.log('challenge #3 -- app.js loaded!');
 
  var app = angular.module("appThree", []);
  app.controller("infoController", infoController)
+app.controller("loanController", loanController) 
 
  function infoController(){
    this.name = "Jon Doe"; // default name
    this.height = 5.6; // default height in feet
  }
- 
+
 function loanController(){
   this.amount = 1000;
   this.apr = 3.5;
